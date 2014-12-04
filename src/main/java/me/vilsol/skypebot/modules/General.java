e13d4ab0492c0c5ab7970e676fd9b88faf2c9bb8
Jarrett <SirCuddles@iCloud.com>
2014-12-03 17:41:58 -0800

    Add Command "dreamincode"

--- src/main/java/me/vilsol/skypebot/modules/General.java
+++ src/main/java/me/vilsol/skypebot/modules/General.java
@@ -207,4 +207,11 @@ public class General implements Module {
         }
     }
 
+    @Command(name = "dreamincode")
+    public static void cmddreamincode(ChatMessage chat){
+        String[] options = new String[] {"No, Im not interested in having a girlfriend I find it a tremendous waste of time.", "Hi, my name is Santiago Gonzalez and I'm 14 and I like to program.", "Im fluent in a dozen different programming languages.", "Thousands of people have downloaded my apps for the mac, iphone, and ipad.", "I will be 16 when I graduate college and 17 when I finish my masters.", "I really like learning, I find it as essential as eating.", "Dr. Bakos: I often have this disease which I call long line-itus.", "Dr. Bakos: Are you eager enough just to write down a slump of code, or is the code itself a artistic median?", "Beutaiful code is short and conzied.", "Sometimes when I goto sleep im stuck with that annoying bug I cannot fix, and in my dreams I see myself programming. When I wake up I have the solution!", "One of the main reasons I started developing apps was to help people what they want to do like decorate a christmas tree.", "I really like to crochet.", "I make good website http://slgonzalez.com/"};
+        int chosen = new Random().nextInt(options.length);
+        R.s(chosen);
+    }
+
 }
