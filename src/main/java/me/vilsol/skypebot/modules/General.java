311d1ba490d4a1c6c33ed4cbb674b5a62ea8a63c
Luke Anderson <stuntguy3000@gmail.com>
2014-12-08 11:08:41 +1030

    Go away, Nobody likes you
    Endorsed by @mazenmc

--- src/main/java/me/vilsol/skypebot/modules/General.java
+++ src/main/java/me/vilsol/skypebot/modules/General.java
@@ -334,25 +334,4 @@ public class General implements Module {
         }
     }
 
-    @Command(name = "yarr")
-    public static void cmdYarr(ChatMessage chat){
-        String[] text = new String[]{"What in Davy Jones’ locker did ye just bark at me, ye scurvy bilgerat?",
-                "I’ll have ye know I be the meanest cutthroat on the seven seas, and I’ve led numerous raids on fishing villages, and raped over 300 wenches.",
-                "I be trained in hit-and-run pillaging and be the deadliest with a pistol of all the captains on the high seas.",
-                "Ye be nothing to me but another source o’ swag.",
-                "I’ll have yer guts for garters and keel haul ye like never been done before, hear me true.",
-                "You think ye can hide behind your newfangled computing device?",
-                "Think twice on that, scallywag.",
-                "As we parley I be contacting my secret network o’ pirates across the sea and yer port is being tracked right now so ye better prepare for the typhoon, weevil.",
-                "The kind o’ monsoon that’ll wipe ye off the map.",
-                "You’re sharkbait, fool. I can sail anywhere, in any waters, and can kill ye in o’er seven hundred ways, and that be just with me hook and fist.",
-                "Not only do I be top o’ the line with a cutlass, but I have an entire pirate fleet at my beck and call and I’ll damned sure use it all to wipe yer arse off o’ the world, ye dog.",
-                "If only ye had had the foresight to know what devilish wrath your jibe was about to incur, ye might have belayed the comment.",
-                "But ye couldn’t, ye didn’t, and now ye’ll pay the ultimate toll, you buffoon.",
-                "I’ll shit fury all over ye and ye’ll drown in the depths o’ it.",
-                "You’re fish food now."};
-
-        R.s(text);
-    }
-
 }
