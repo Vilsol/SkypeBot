3144f81a0fcdb19a55a6d90dc46806acd47cb9da
Luke Anderson <stuntguy3000@gmail.com>
2015-01-22 14:56:42 +1030

    Fix admin list

--- src/main/java/io/mazenmc/skypebot/utils/Resource.java
+++ src/main/java/io/mazenmc/skypebot/utils/Resource.java
@@ -5,7 +5,7 @@ import io.mazenmc.skypebot.SkypeBot;
 public class Resource {
 
     public static final String COMMAND_PREFIX = "@";
-    public static final String[] GROUP_ADMINS = {"stuntguy3000", "gnosticjade", "heycallmethemezand"};
+    public static final String[] GROUP_ADMINS = {"stuntguy3000", "gnosticjade", "heycallmethemezand", "itssircuddles"};
     public static final String KEY_GITHUB = Utils.readFirstLine("key_github");
     public static final String KEY_TRIDENT = Utils.readFirstLine("key_trident");
     public static final String REGEX_ALL = "(.+)";
@@ -13,7 +13,7 @@ public class Resource {
     public static final String REGEX_INT = "(-?[0-9]+)";
     public static final String REGEX_WORD = "(\\b+)";
     public static final String URBAN_DICTIONARY_URL = "http://www.urbandictionary.com/define.php?term=";
-    public static final String VERSION = "1.3.5";
+    public static final String VERSION = "1.3.6";
 
     public static void sendMessage(String message) {
         SkypeBot.getInstance().sendMessage(message);
