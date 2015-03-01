a0dd8638499ab5c6dbaaee572b547c751032ff84
Luke Anderson <stuntguy3000@gmail.com>
2015-03-01 14:58:35 +1030

    Remove un-needed

--- src/main/java/io/mazenmc/skypebot/utils/Resource.java
+++ src/main/java/io/mazenmc/skypebot/utils/Resource.java
@@ -23,8 +23,7 @@ public class Resource {
     public static final Pattern SPOTIFY_HTTP_REGEX = Pattern.compile("open\\.spotify\\.com/track/([A-z0-9]){22}");
     public static final Pattern SPOTIFY_URI_REGEX = Pattern.compile("spotify:track:([A-z0-9]){22}");
     public static final Pattern TWITTER_REGEX = Pattern.compile("twitter\\.com\\/([A-z0-9]+)\\/status\\/([0-9]{18})");
-    public static final Pattern IMAGE_REGEX = Pattern.compile("^https?://(?:[a-z\\-]+\\.)+[a-z]{2,6}(?:/[^/#?]+)+\\.(?:jpg|gif|png)$");
-
+   
     private static Map<String, Callback<String>> callbacks = new HashMap<>();
 
     public static void sendMessage(String message) {
