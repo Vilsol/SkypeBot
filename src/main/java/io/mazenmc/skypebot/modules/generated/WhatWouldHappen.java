57f39040f5f49fa858770a2fba15a3b076e88cd3
mattrick <matthewhighrollers@gmail.com>
2015-03-01 14:42:34 -0800

    Add more @whatwouldhappen effects

--- src/main/java/io/mazenmc/skypebot/modules/generated/WhatWouldHappen.java
+++ src/main/java/io/mazenmc/skypebot/modules/generated/WhatWouldHappen.java
@@ -13,7 +13,7 @@ public class WhatWouldHappen implements Module {
 
     private static final String[] OPTIONS = new String[] {
             "World Domination would occur",
-            "An amazing and beautiful thing would happen",
+            "an amazing and beautiful thing would happen",
             "Mazen would die",
             "ChipDev would become president",
             "codename_B would quit Hypixel",
@@ -41,13 +41,27 @@ public class WhatWouldHappen implements Module {
             "Garrison would make a joke about people dying in Iraq",
             "Bill Gates would come out gay",
             "Steve Wozniak would release to the world that Steve Jobs is still alive",
-            "The Nokia 3310 would break",
-            "North Korea would Nuke [countries]",
-            "[countries] would start a war against [countries]"
+            "the Nokia 3310 would break",
+            "North Korea would nuke [countries]",
+            "[countries] would start a war against [countries]",
+            "Jade would stop caring about being pinged",
+            "Mazen would lose his virginity",
+            "Mazen would stop making apps to decorate Christmas trees",
+            "md_5 would sell Spigot to Mojang without telling anyone",
+            "md_5 would go to work for Mojang",
+            "rowtn would have good internet",
+            "Mazen's Skype Chat would be killed by Lizard Squad (again)",
+            "[countries] would nuke [countries]",
+            "ADDITIONAL PYLONS WOULD BE CONSTRUCTED",
+            "mazen.bot would die",
+            "[member] would kill [member] with [objects]",
+            "[member] would ejactulate [objects]"
     };
 
     private static final HashMap<String, String[]> DATA = new HashMap<String, String[]>() {{
-        put("countries", new String[]{"Argentina", "Brazil", "Russia", "USA", "Canada", "China", "North Korea", "France"});
+        put("countries", new String[]{"Argentina", "Brazil", "Russia", "USA", "Canada", "China", "North Korea", "France", "Australia"});
+        put("members", new String[]{"Mazen", "Luke", "rowtn", "Troll", "Garrison", "Erik", "Filip", "Jade", "mattrick", "Vilsol", "Mark"});
+        put("objects", new String[]{"a dildo", "a knife", "a Mac", "an iPhone", "an anvil", "spiders", "a kangaroo", "a panda", "a member of ISIS", "a velociraptor", "a nerf gun"});
     }};
 
     @Command(name = "whatwouldhappen")
