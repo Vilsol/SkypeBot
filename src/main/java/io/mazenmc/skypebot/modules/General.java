93031cca20b374f1071e1959d6625c1582402e0f
Mazen Kotb <email@mazenmc.io>
2015-11-30 17:09:02 -0800

    dream in code updates

--- src/main/java/io/mazenmc/skypebot/modules/General.java
+++ src/main/java/io/mazenmc/skypebot/modules/General.java
@@ -644,20 +644,29 @@ public class General implements Module {
 
     @Command(name = "dreamincode", alias = {"whatwouldmazensay"})
     public static void cmddreamincode(ChatMessage chat) throws SkypeException {
-        String[] options = new String[]{"No, I'm not interested in having a girlfriend I find it a tremendous waste of time.",
-                "Hi, my name is Santiago Gonzalez and I'm 14 and I like to program.",
-                "I'm fluent in a dozen different programming languages.",
-                "Thousands of people have downloaded my apps for the Mac, iPhone, and iPad.",
-                "I will be 16 when I graduate college and 17 when I finish my masters.",
-                "I really like learning, I find it as essential as eating.",
-                "Dr. Bakos: I often have this disease which I call long line-itus.",
-                "Dr. Bakos: Are you eager enough just to write down a slump of code, or is the code itself a artistic medium?",
-                "Beautiful code is short and concise.",
-                "Sometimes when I go to sleep I'm stuck with that annoying bug I cannot fix, and in my dreams I see myself programming. " +
-                        "When I wake up I have the solution!",
-                        "One of the main reasons I started developing apps was to help people what they want to do like decorate a christmas tree.",
-                        "I really like to crochet.",
-        "I made a good website http://slgonzalez.com/"};
+        String[] options = new String[] {
+            "No, I'm not interested in having a girlfriend I find it a tremendous waste of time.",
+            "Hi, my name is Santiago Gonzalez and I'm 14 and I like to program.",
+            "I'm fluent in a dozen different programming languages.",
+            "Thousands of people have downloaded my apps for the Mac, iPhone, and iPad.",
+            "I will be 16 when I graduate college and 17 when I finish my masters.",
+            "I really like learning, I find it as essential as eating.",
+            "Dr. Bakos: I often have this disease which I call long line-itus.",
+            "Dr. Bakos: Are you eager enough just to write down a slump of code, or is the code itself a artistic medium?",
+            "Beautiful code is short and concise.",
+            "When you're around a certain race enough, you tend to become like them physically and mentally; soon enough I'll be driving terribly and doing math competitions",
+            "Sometimes when I go to sleep I'm stuck with that annoying bug I cannot fix, and in my dreams I see myself programming. \nWhen I wake up I have the solution!",
+            "One of the main reasons I started developing apps was to help people what they want to do like decorate a christmas tree.",
+            "I really like to crochet.",
+            "I made a good website http://slgonzalez.com/",
+            "If that was my sister I'd eat her.",
+            "https://s.mzn.pw/08bG9Ti.png",
+            "ISIS more like waswas",
+            "eat less food habibi @aaomidi",
+            "Are aero bars forming in my ears?",
+            "Less halawa more carrot @aaomidi",
+            "I live in Vancouver, which is practically Asia. Soon enough British Columbia will be renamed to Asian Columbia"
+        };
         int chosen = ThreadLocalRandom.current().nextInt(options.length);
         Resource.sendMessage(chat, options[chosen]);
     }
