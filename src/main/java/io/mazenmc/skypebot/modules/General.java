80a38ac38b2ab70697251fe286268dab9701eb28
Nick Robson <robson.nicholas1@gmail.com>
2016-01-27 15:47:01 +0800

    oops

--- src/main/java/io/mazenmc/skypebot/modules/General.java
+++ src/main/java/io/mazenmc/skypebot/modules/General.java
@@ -2,7 +2,7 @@ package io.mazenmc.skypebot.modules;
 
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;b
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.customsearch.Customsearch;
 import com.google.api.services.customsearch.model.Result;
