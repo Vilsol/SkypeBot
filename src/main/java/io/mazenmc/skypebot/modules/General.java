4f260c55618a16ae14d188de66921bc7698efae4
Nick Robson <robson.nicholas1@gmail.com>
2016-01-27 15:46:30 +0800

    get rid of that shitty thing

--- src/main/java/io/mazenmc/skypebot/modules/General.java
+++ src/main/java/io/mazenmc/skypebot/modules/General.java
@@ -2,7 +2,7 @@ package io.mazenmc.skypebot.modules;
 
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonFactory;b
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.customsearch.Customsearch;
 import com.google.api.services.customsearch.model.Result;
@@ -742,11 +742,6 @@ public class General implements Module {
     @Command(name = "basoon")
     public static void cmdBasoon(ReceivedMessage chat) throws Exception {
         Resource.sendMessage(chat, "Blows Air Strongly Out Of Nose");
-    }
-
-    @Command(name = "(?i)lol", command = false, exact = false)
-    public static void lolDetect(ReceivedMessage chat) throws Exception {
-        Resource.sendMessage("basoon*");
     }
 
     @Command(name = "(?i)savage", command = false, alias = "(?i)brutal")
